1398f1ac5810526236cef063d179c5866786c007
Corey Redding and Leah Sherrell <corey@>
2017-04-24 11:02:49 -0700

    add the subtract function

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,12 @@ var add = function(number1, number2){
 	return number1 + number2;
 };
 
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+
 var number1 = parseInt(prompt("Enter a Number:"));
 
-var number2 = parseInt(prompt("Enter a Number:"));
+var number2 = parseInt(prompt("Enter another Number:"));
 
-alert(add(number1,number2));
+// alert(add(number1,number2));
