da059544806601b7865a19a131d1406202fe3553
Corey Redding and Leah Sherrell <leah@>
2017-04-24 11:05:38 -0700

    add divide function

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ var multiply = function(number1, number2){
 	return number1 * number2;
 };
 
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+
 var multiply = function(number1, number2){
 	return number1 + number2;
 };
